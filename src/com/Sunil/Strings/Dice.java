17371764935b052a59ea4100180ab77be0eb3f98
Sunil Ballani <ballanisunil123@gmail.com>
2022-06-24 22:19:47 +0530

    Commited a change in Dice question

--- src/com/Sunil/Strings/Dice.java
+++ src/com/Sunil/Strings/Dice.java
@@ -1,8 +1,10 @@
 package com.Sunil.Strings;
 
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args) {
-        dice("", 4);
+        System.out.println(diceFaceList("", 7, 9));
     }
     static void dice(String p, int target){
         if(target == 0){
@@ -21,5 +23,17 @@ public class Dice {
         for(int i = 1; i<=face && i<=target; i++){
             diceFace(p + i, target-i, face);
         }
+    }
+    static ArrayList<String> diceFaceList(String p, int target, int face){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String>list = new ArrayList<>();
+        for(int i = 1; i<=face && i<=target; i++){
+            list.addAll(diceFaceList(p + i, target-i, face));
+        }
+        return list;
     }
 }
